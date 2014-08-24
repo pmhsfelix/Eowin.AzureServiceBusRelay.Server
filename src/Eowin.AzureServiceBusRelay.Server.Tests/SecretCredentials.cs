92e8c3287456731e1c919572c9be9e2f3cbb16b6
Pedro Felix <pmhsfelix@gmail.com>
2014-08-24 23:26:36 +0100

    Adds SecretCredentials class that reads test credencials from the environment

---
+++ src/Eowin.AzureServiceBusRelay.Server.Tests/SecretCredentials.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Eowin.AzureServiceBusRelay.Server.Tests
+{
+    public class SecretCredentials
+    {
+        public static string ServiceBusAddress
+        {
+            get
+            {
+                var addr = Environment.GetEnvironmentVariable("ServiceBusAddress");
+                if(string.IsNullOrWhiteSpace(addr)) throw new InvalidOperationException("ServiceBusAddress is not defined");
+                return addr;
+            }
+        }
+
+        public static string Secret
+        {
+            get
+            {
+                var secret = Environment.GetEnvironmentVariable("ServiceBusSecret");
+                if (string.IsNullOrWhiteSpace(secret)) throw new InvalidOperationException("ServiceBusSecret is not defined");
+                return secret;
+            }
+        }
+    }
+}
