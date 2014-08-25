c7ba16140706780aac0b083c3940d7d8ae18bd74
Pedro Felix <pmhsfelix@gmail.com>
2014-08-25 19:38:47 +0100

    Adds an example using NancyFx

---
+++ src/Eowin.AzureServiceBusRelay.Server.Examples.NancyFx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Eowin.AzureServiceBusRelay.Server.Tests;
+using Nancy;
+using Owin;
+
+namespace Eowin.AzureServiceBusRelay.Server.Examples.NancyFx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string address = "https://webapibook.servicebus.windows.net/nancy/";
+            var sbConfig = new AzureServiceBusOwinServiceConfiguration(
+                issuerName: "owner",
+                issuerSecret: SecretCredentials.Secret,
+                address: address);
+            var server = AzureServiceBusOwinServer.Create(sbConfig, app =>
+            {
+                app.UseNancy();
+            });
+            Console.WriteLine("Server is listening at {0}", address);
+            Console.ReadKey();
+        }
+    }
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/nancy"] = x => "hello from Nancy in the cloud";
+        }
+    }
+}
